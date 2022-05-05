fdfa6b7c70708fcbbf0744a8d04b733905de36bc
valentyn <valik@esmedia.es>
2022-05-05 16:08:41 +0300

    init

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+from setuptools import dist
+from setuptools import setup
+from setuptools_rust import Binding, RustExtension
+
+dist.Distribution().fetch_build_eggs(['setuptools_rust'])
+
+setup(
+    name="fib_rs",
+    version="0.1",
+    rust_extensions=[RustExtension(
+        ".fib_rs.fib_rs",
+        path="Cargo.toml", binding=Binding.PyO3)],
+    packages=["fib_rs"],
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Programming Language :: Rust",
+        "Operating System :: POSIX",
+        "Operating System :: MacOS :: MacOS X",
+    ],
+    zip_safe=False,
+)
